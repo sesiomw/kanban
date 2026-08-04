7215c4c39c654ddc06a6f7ee593aee460df57a14
Your Name <you@example.com>
2026-08-04 15:00:42 -0300

    Base da função criarTarefa

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const containerAdicionar = document.querySelector('adicionar');
+const inputAdicionar = document.querySelector('input');
+const buttonAdicionar = document.querySelector('button');
+const containerTodo = document.querySelector('.todo-column');
+const containerDoing = document.querySelector('.doing-column');
+const containerDone = document.querySelector('.done-column');
+const templateTarefa = document.querySelector('template');
 
+function criarTarefa(texto) {
+    if (texto.trim() === '') return;
+    const tarefa = templateTarefa.content.cloneNode(true);
+    const spanTitle = tarefa.querySelector('span');
+    const buttonExcluir = tarefa.querySelector('.excluir');
+    spanTitle.textContent = texto;
+    buttonExcluir.onclick = () => {buttonExcluir.closest('.task-card').remove()};
+    containerTodo.appendChild(tarefa);
+}
